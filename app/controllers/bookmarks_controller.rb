07ef4060253e26522962f1fb28f030d419f9a692
aspsa <123aspsa123@gmail.com>
2015-11-25 03:02:05 +0000

    [In-progress] Create a bookmark.

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -4,10 +4,37 @@ class BookmarksController < ApplicationController
   end
 
   def new
-    @topic = Topic.new
+    @topic = Topic.find(params[:topic_id])
     @bookmark = @topic.bookmarks.new
+  end
+  
+  def create
+    @topic = Topic.find(params[:topic_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    
+    if @bookmork.save
+      flash[:notice] = "Bookmark saved."
+      redirect_to topics_path
+    else
+      flash[:error] = "Error: Bookmark not saved."
+      render "new"
+    end
   end
 
   def edit
   end
+  
+  def update
+  
+  end
+  
+  def destroy
+  
+  end
+  
+  private
+  
+    def bookmark_params
+      params.require(:bookmark).permit(:url)
+    end
 end
