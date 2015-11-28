825007ca09ca59bcaa18229c892de7e064dbdf62
aspsa <123aspsa123@gmail.com>
2015-11-28 18:51:23 +0000

    Finish Checkpoint 6 - Bookmarks CRUD

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -14,7 +14,8 @@ class BookmarksController < ApplicationController
 
     if @bookmark.save
       flash[:notice] = "Bookmark saved."
-      redirect_to topic_bookmark_path
+      #redirect_to topic_bookmark_path
+      redirect_to @topic
     else
       flash[:error] = "Error: Bookmark not saved."
       render "new"
@@ -23,23 +24,35 @@ class BookmarksController < ApplicationController
 
   def edit
     @topic = Topic.find(params[:topic_id])
-    @bookmark = @topic.bookmarks(params[:id])
+    @bookmark = @topic.bookmarks.find(params[:id])
   end
   
   def update
-  
+    @topic = Topic.find(params[:topic_id])
+    @bookmark = @topic.bookmarks.find(params[:id])
+    
+    if @bookmark.update_attributes(bookmark_params)
+      flash[:notice] = "Bookmark was updated."
+      redirect_to @topic
+    else
+      flash[:error] = "There was an error updating this bookmark. Please try again."
+      render :edit
+    end
   end
   
   def destroy
     @topic = Topic.find(params[:topic_id])
-    @bookmark = @topic.bookmarks(params[:id])
+    #@bookmark = @topic.bookmarks(params[:id])
+    @bookmark = @topic.bookmarks.find(params[:id])
     
     if @bookmark.destroy
       flash[:notice] = "Bookmark deleted." # Add flash messaging to applications.html.erb
-      redirect_to topics_path
+      #redirect_to topics_path
+      redirect_to @topic
     else
       flash[:error] = "Error: Bookmark not deleted."
       #render "topics/index"
+      redirect_to @topic
     end
   end
   
